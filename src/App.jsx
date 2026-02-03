c828d870f936628e374729cb552a3ee4d30193d6
Fathi <fathisyameer22@gmail.com>
2026-02-03 16:40:41 +0800

    -add result layout for anime card

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
 
 const [inputSearch,setInputSearch] = useState('')
 const [dataResultSearch,setDataResultSearch] = useState([]);
+//untuk tengok ayat panjang//
+
+const [isPanjang,setIsPanjang] = useState(false);
+
 
   const handleApi = async ()=>{
     const data = await animeApi();
@@ -38,6 +42,12 @@ const [dataResultSearch,setDataResultSearch] = useState([]);
 // const handleClick = (e)=>{
 // e.preventDefault() 
 
+const clickMore = ()=>{
+  console.log('click')
+  setIsPanjang(!isPanjang)
+}
+
+
 
 
 // }
@@ -55,18 +65,41 @@ const [dataResultSearch,setDataResultSearch] = useState([]);
   dataResultSearch.map((anime)=>{
 
 
-    return  <div className="flex flex-col justify-center items-center" key={anime.id}>
-          <h1 className="text-[60px]">{anime.title}</h1>
-          <div>
-            <img src={anime.images.jpg.large_image_url} alt="" />
-            <div>
-              <p>Release Year : {anime.year}</p>
-              <p>Rating: {anime.rating}</p>
-              <p>Rank :{anime.rank}</p>
-              <p>Score : {anime.score}</p>
+    return  <div className="flex flex-col justify-center items-center  " >
+
+      
+
+ <h1 className="text-[60px]">{anime.title}</h1>
+
+<div className=" shadow-2xl  p-4 flex justify-around gap-4 w-[1200px] rounded-2xl">
+
+
+          <div className=" flex flex-col items-center">
+              <img className="h-[600px]" src={anime.images.jpg.large_image_url} alt="" />
+              <p className={`max-w-[600px] ${isPanjang?  '' : 'truncate' } mt-[5%] px-2 text-gray-400`}>{anime.synopsis}</p>
+              <button onClick={clickMore} className="text-red-300 border bg-black hover:text-red-500">{isPanjang ? 'close' : 'Read More'}</button>
+            
+          </div>
+           
+
+
+            <div className=" text-2xl p-2 text-[30px] flex flex-col gap-6 ">
+              <p>Release Year : <span className="text-gray-400" > {anime.year}</span> </p>
+              <p>Rating: <span className="text-gray-400" >  {anime.rating} </span> </p>
+              <p>Rank : <span className="text-gray-400" > {anime.rank} </span> </p>
+              <p>Score : <span className="text-gray-400" >  {anime.score} </span> </p>
+              <p>Popularity : <span className="text-gray-400" >  {anime.popularity} </span> </p>
+              <p>Status : <span className="text-gray-400" >  {anime.status} </span> </p>
+              <p>Source : <span className="text-gray-400" >  {anime.source} </span> </p>
+              <p>Season : <span className="text-gray-400" >  {anime.season} </span> </p>
+              <p>Duration : <span className="text-gray-400" >  {anime.duration} </span> </p>
              
             </div>
-          </div>
+        
+      
+         
+    </div>
+
     </div>
 
 
